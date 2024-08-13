4a065990ad53fbc6c83444acb884eb13aae24185
Harsh Tiwari <92631120+Im-HarshTiwari@users.noreply.github.com>
2024-08-13 20:27:47 +0530

    Update FizzBuzz.java

--- question/FizzBuzz.java
+++ question/FizzBuzz.java
@@ -10,7 +10,7 @@ public class FizzBuzz {
                     System.out.println("Fizz");
                     } else if (i % 5 == 0) {
                         System.out.println("Buzz");
-                        }// else { System.out.println(i);
+                        }// else { System.out.println(i); ;
                        // }
                         }
                     }
