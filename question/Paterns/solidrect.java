0944b4bd729aec7a93bce17b4237b02f7b44aef1
Harsh Tiwari <92631120+Im-HarshTiwari@users.noreply.github.com>
2024-08-19 23:41:44 +0530

    Update solidrect.java

--- question/Paterns/solidrect.java
+++ question/Paterns/solidrect.java
@@ -6,7 +6,7 @@ public class solidrect {
        int n = 4;
        int m = 5;
        for(int i = 0 ; i<=n ; i++){       //outer loop
-        for(int j = 0 ; j<=m ; j++){
+        for(int j = 0 ; j<=m ; j++){      //innner loop
             System.out.print(" * ");      //here should not be println becoz it will print in sequential wise
        }
           System.out.println(" "); //for new line or space
