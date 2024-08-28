3f2ad44df03d8d964e8dd53db48bc78cb5cd914a
Harsh Tiwari <92631120+Im-HarshTiwari@users.noreply.github.com>
2024-08-28 20:24:00 +0530

    Update readndprint.java

--- question/readndprint.java
+++ question/readndprint.java
@@ -4,7 +4,7 @@ public class readndprint {
     public static void main(String[] args) {
         // Create a file object
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value in int. That you want to print");  //print
+        System.out.println("Enter the value in int. That you want to print");  //print ..
         int num = sc.nextInt();
         System.out.println("You entered: " + num);
     }
