8d7b594252c7318e558fe51f4c5565f7b5b0bcdd
Im-HarshTiwari <harshtiwari202000@gmail.com>
2024-09-03 23:35:55 +0530

    update

--- Recursion/xPower.java
+++ Recursion/xPower.java
@@ -6,7 +6,7 @@ public class xPower {           //stack height max is n
     if (n==0){              //base case 1
         return 1;
     }
-    if (x==0){               //base case 2
+    if (x==0){               //base case 2..
         return 0;
     }
       int xPowern = calPow(x , n-1);       //work
