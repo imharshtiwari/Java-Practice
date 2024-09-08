4523f538618214bab5abffa25cd62769264aff37
Im-HarshTiwari <harshtiwari202000@gmail.com>
2024-09-08 08:37:31 +0530

    Added file

---
+++ Collection-Framework/ArrayList/listiterator.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+
+public class listiterator {
+                    
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();   //<integer> is a type of Integer object 
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+     
+      System.out.println(list); 
+      
+      //iterator to traverse the list 
+      
+    
+    }
+}
