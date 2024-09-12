cd3b1a594231683d87829bb0380454147853fab3
Im-HarshTiwari <harshtiwari202000@gmail.com>
2024-09-12 08:01:14 +0530

    Added Ploymorphism

---
+++ Pillars-of-OOPS-In-Java/Polymorphism.java
@@ -0,0 +1,80 @@
+
+
+
+ interface MyCamera{
+          void takePicture();
+          void recordVideo();
+          void editVideo();
+          void shareVideo();
+          void deleteVideo();
+          void viewVideo();
+          
+          private void update(){
+            System.out.println("Updated Camera Methods");
+          }     
+     default void recordVideo4k(){
+        System.out.println("Recoring video in 4K...");
+    }
+ }
+ interface MyWifi{
+    String [] getNetwork(); 
+    void connectToNetwork(String network);
+     
+
+ }
+ class MyCellPhone{
+    void callNumber(int number){
+    System.out.println("calling" + number);
+    };
+    void pickCall(){
+        System.out.println("Connecting..." );
+    }
+      }
+      class MySmartPhone extends MyCellPhone implements MyCamera, MyWifi {
+
+         public void takePicture() {
+             System.out.println("Taking Picture...");
+         }
+     
+         public void recordVideo() {
+             System.out.println("Recording Video...");
+         }
+     
+         public void editVideo() {
+             System.out.println("Editing Video...");
+         }
+     
+         public void shareVideo() {
+             System.out.println("Sharing Video...");
+         }
+     
+         public void deleteVideo() {
+             System.out.println("Deleting Video...");
+         }
+     
+         public void viewVideo() {
+             System.out.println("Viewing Video...");
+         }
+     
+         public String[] getNetwork() {
+             return new String[]{"Network 1", "Network 2", "Network 3"};
+         }
+     
+         public void connectToNetwork(String network) {
+             System.out.println("Connecting to network: " + network);
+         }
+     }
+public class Polymorphism {
+    
+
+    public static void main(String[] args) {
+      // TODO code application logic here
+      MyCamera cam = new MySmartPhone();
+      cam.takePicture();
+      cam.recordVideo();
+      cam.recordVideo4k();
+
+     
+
+}
+}
