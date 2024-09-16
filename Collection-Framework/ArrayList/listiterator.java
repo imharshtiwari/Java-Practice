32c840e31ee658c46e647743e5866ca4ab2627f8
Im-HarshTiwari <harshtiwari202000@gmail.com>
2024-09-16 18:28:15 +0530

    Update

--- Collection-Framework/ArrayList/listiterator.java
+++ Collection-Framework/ArrayList/listiterator.java
@@ -9,7 +9,7 @@ public class listiterator {
         list.add(1); //e is reffered as a object , as because it is implicit conversion 
         list.add(2);
         list.add(3);  //implicit conversion , they are treadted as objects insted of data
-        list.add(4);  //by default it will increase by 50% 
+        list.add(4);  //by default it will increase by 50% ..
         
      
       System.out.println(list); 
