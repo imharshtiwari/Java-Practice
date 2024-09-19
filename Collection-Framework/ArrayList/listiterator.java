3a668159466d3a7a410b0bd300b68e887bf70f96
Harsh Tiwari <92631120+Im-HarshTiwari@users.noreply.github.com>
2024-09-19 23:15:00 +0530

    Update listiterator.java

--- Collection-Framework/ArrayList/listiterator.java
+++ Collection-Framework/ArrayList/listiterator.java
@@ -6,7 +6,7 @@ public class listiterator {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>(); //by default it will be store 10 elements
            //<integer> is a type of Integer object 
-        list.add(1); //e is reffered as a object , as because it is implicit conversion 
+        list.add(1); //e is reffered as a object , as because it is implicit conversion ..
         list.add(2);
         list.add(3);  //implicit conversion , they are treadted as objects insted of data
         list.add(4);  //by default it will increase by 50% ..
